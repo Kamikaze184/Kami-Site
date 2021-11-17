5e4adcd4f0f025814d86c16e0c09df4f2b7a20a2
Kamikaze_184 <alan.filho@hotmail.com>
2021-11-17 02:56:19 -0300

    v2.0.4 - Criando arquivos na vercel

--- db/index.js
+++ db/index.js
@@ -1,24 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-const dbPath = path.join(__dirname, "json", 'db.json')
+const dbPath = path.join(process.env.local)
 var firstS = true
 
 module.exports = class json_database {
     constructor() {
         if (firstS) {
-            if (!fs.existsSync(dbPath)) {
-                fs.writeFileSync(dbPath, "{}", { flag: "w+" }, (err) => {
-                    if (err) {
-                        throw err;
-                    }
-                })
-            }
+            fs.writeFileSync(dbPath, "{}", { flag: "w+" }, (err) => {
+                if (err) {
+                    throw err;
+                }
+            })
             firstS = false
         }
     }
 
     set(key, value) {
-        var db = require('./json/db.json')
+        var db = require(dbPath)
         db[key] = { content: value }
 
         fs.writeFileSync(dbPath, JSON.stringify(db), { flag: "w+" }, (err) => {
@@ -34,7 +32,7 @@ module.exports = class json_database {
         let toReturn
 
         try {
-            toReturn = require('./json/db.json')[key].content
+            toReturn = require(dbPath)[key].content
         }
         catch (err) {
             toReturn = undefined
@@ -44,7 +42,7 @@ module.exports = class json_database {
     }
 
     delete(key) {
-        var db = require('./json/db.json')
+        var db = require(dbPath)
         db[key] = undefined
 
         fs.writeFileSync(dbPath, JSON.stringify(db), { flag: "w+" }, (err) => {
@@ -57,13 +55,13 @@ module.exports = class json_database {
     }
 
     has(key) {
-        const db = require('./json/db.json')
+        const db = require(dbPath)
         if (db[key].content) { return true }
         else { return false }
     }
 
     length() {
-        const db = require('./json/db.json')
+        const db = require(dbPath)
         return db.size
     }
 }
