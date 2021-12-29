24aa789c6be2082c67af3525b6785b10041c7d13
Kamikaze_184 <alan.filho@hotmail.com>
2021-12-29 12:16:29 -0300

    v2.7.2 - BotStatus count up

---
+++ public/scripts/countUp.js
@@ -0,0 +1,33 @@
+const serverCount = document.getElementById('serverCount');
+const userCount = document.getElementById('userCount');
+const fichasCount = document.getElementById('fichasCount');
+const commandsTotal = document.getElementById('commandsTotal');
+
+const severCountValue = document.currentScript.getAttribute('serverCount');
+const userCountValue = document.currentScript.getAttribute('userCount');
+const fichasCountValue = document.currentScript.getAttribute('fichasCount');
+const commandsTotalValue = document.currentScript.getAttribute('commandsTotal');
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function count(local, value) {
+    var speed = Math.ceil(((value * 0.0001) + ((value * 1.3) / 500)))
+    let count = 0;
+
+    while (count < value) {
+        local.innerHTML = count;
+        count += speed;
+        await sleep(10);
+    }
+
+    if (count >= value) {
+        local.innerHTML = value;
+    }
+}
+
+count(commandsTotal, commandsTotalValue);
+count(userCount, userCountValue);
+count(fichasCount, fichasCountValue);
+count(serverCount, severCountValue);
