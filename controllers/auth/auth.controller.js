09fdc37434be7d7480049917a2ee43da6c8d8428
Kamikaze_184 <alan.filho@hotmail.com>
2022-02-16 19:18:09 -0300

    v3.1.0-beta - Interface melhorada para mobile

--- controllers/auth/auth.controller.js
+++ controllers/auth/auth.controller.js
@@ -5,17 +5,21 @@ const authServices = require('../../services/auth.service')
 const services = new authServices()
 
 routes.get("/login", (req, res) => {
-    // const session = req.session
-    // session.user = {
-    //     id: '252868463150759961',
-    //     username: 'Kamikaze_184',
-    //     avatar: '347aa6655964484ff3f10e74c1170c1c',
-    //     discriminator: '7417',
-    //     avatarURL: 'https://cdn.discordapp.com/avatars/252868463150759961/347aa6655964484ff3f10e74c1170c1c.png?size=128',
-    // }
-    // session.validation = process.env.validation
-    // res.redirect("/")
-    res.redirect(`${process.env.oauthUri}`)
+    if (process.env.deploy == "development") {
+        const session = req.session
+        session.user = {
+            id: '252868463150759961',
+            username: 'Kamikaze_184',
+            avatar: '347aa6655964484ff3f10e74c1170c1c',
+            discriminator: '7417',
+            avatarURL: 'https://cdn.discordapp.com/avatars/252868463150759961/347aa6655964484ff3f10e74c1170c1c.png?size=128',
+        }
+        session.validation = process.env.validation
+        res.redirect('/')
+    }
+    else if (process.env.deploy == "production") {
+        res.redirect(`${process.env.oauthUri}`)
+    }
 })
 
 routes.get("/logout", (req, res) => {
