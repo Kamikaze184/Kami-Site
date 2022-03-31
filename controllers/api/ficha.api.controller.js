db59d58eeceead5da38b19fb358f1e071f585788
Kamikaze_184 <alan.filho@hotmail.com>
2022-03-31 09:37:04 -0300

    v3.6.7-beta - Redirect no login para a pagina de destino ou mesma pagina

--- controllers/api/ficha.api.controller.js
+++ controllers/api/ficha.api.controller.js
@@ -127,23 +127,32 @@ module.exports = class api_ficha_controller {
         })
 
         routes.post("/password", async (req, res) => {
-            try {
-                const response = await services.getFichaWithPassword(req.body)
+            if (req.headers.authorization == process.env.validation) {
+                try {
+                    const response = await services.getFichaWithPassword(req.body)
 
-                if (response.status === 200) {
-                    req.session.access = {
-                         [`${req.body.id}${req.body.nomerpg}`]: req.body.senha
+                    if (response.status === 200) {
+                        req.session.access = {
+                            [`${req.body.id}${req.body.nomerpg}`]: req.body.senha
+                        }
+                        req.session.cookie.maxAge = 1000 * 60 * 60 * 12
+
+                        res.status(200).json({ id: req.body.id, nomerpg: req.body.nomerpg })
                     }
-                    req.session.cookie.maxAge = 1000 * 60 * 60 * 12
-
-                    res.status(200).json({id: req.body.id, nomerpg: req.body.nomerpg})
+                    else {
+                        res.status(response.status).send(response.data)
+                    }
                 }
-                else {
-                    res.status(response.status).send(response.data)
+                catch (err) {
+                    res.status(500).end()
                 }
             }
-            catch (err) {
-                res.status(500).end()
+            else {
+                res.status(401).json({
+                    status: 401,
+                    title: "Falha na validação",
+                    text: "Este endpoint é somente para o servidor"
+                })
             }
         })
 
