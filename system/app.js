d54d298e675394e2c66c386ef0efb947b5559345
alanfilho184 <alan.filho@hotmail.com>
2022-09-30 23:12:53 -0300

    v4.0.12-beta - Bot rodando na mesma vps do site

--- system/app.js
+++ system/app.js
@@ -220,7 +220,7 @@ module.exports = class App {
 
     setWebSocket() {
         const { io } = require("socket.io-client")
-        const connUrl = process.env.deploy == "development" ? "http://localhost:3005/" : "https://bot.kamiapp.com.br/"
+        const connUrl = "http://localhost:3005/"
 
         const socket = new io(connUrl, {
             reconnectionDelayMax: 5000,
